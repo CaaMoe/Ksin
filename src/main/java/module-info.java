52d9faf46731b913379334dcff76c3e2f889f037
CaaMoe <aeva_d@hotmail.com>
2025-09-11 19:05:22 +0800

    添加对SpongePowered Configurate和Google Guice的依赖

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module Ksin.main {
     requires com.velocitypowered.api;
     requires org.jetbrains.annotations;
     requires org.slf4j;
+    requires org.spongepowered.configurate;
+    requires com.google.guice;
 
     exports moe.caa.multilogin.ksin.api;
 }
