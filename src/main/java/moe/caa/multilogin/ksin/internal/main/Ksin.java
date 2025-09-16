351305ab1e7b890f45c5367c905144836165f3b3
CaaMoe <aeva_d@hotmail.com>
2025-09-16 10:25:29 +0800

    添加bStats

--- src/main/java/moe/caa/multilogin/ksin/internal/main/Ksin.java
+++ src/main/java/moe/caa/multilogin/ksin/internal/main/Ksin.java
@@ -4,6 +4,9 @@ import moe.caa.multilogin.ksin.internal.bootstrap.KsinBootstrap;
 import moe.caa.multilogin.ksin.internal.logger.KLogger;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 public class Ksin {
     public static final @NotNull Ksin INSTANCE = new Ksin();
     public KLogger logger;
@@ -12,9 +15,27 @@ public class Ksin {
     public void enable(@NotNull KsinBootstrap bootstrap) {
         this.bootstrap = bootstrap;
         this.logger = bootstrap.logger;
+
+        setupMetrics();
     }
 
     public void disable() {
 
+    }
+
+
+    private void setupMetrics() {
+        try {
+            Class<?> metricsFactoryClass = Class.forName("org.bstats.velocity.Metrics$Factory");
+
+            Constructor<?> metricsFactoryConstructor = metricsFactoryClass.getConstructors()[0];
+            Object metricsFactory = metricsFactoryConstructor.newInstance(bootstrap.server, bootstrap.logger.handle, bootstrap.dataDirectory);
+
+            Method makeMethod = metricsFactoryClass.getMethod("make", Object.class, int.class);
+            makeMethod.invoke(metricsFactory, bootstrap, 26924);
+            logger.debug("Setup bStats metrics.");
+        } catch (Throwable e) {
+            logger.error("Failed to setup bStats metrics.", e);
+        }
     }
 }
